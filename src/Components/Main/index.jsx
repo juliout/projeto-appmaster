712643e69ac8acd0760494a1799c9fba11e6d35f
juliout <juliocst1993@gmail.com>
2023-06-23 16:01:24 -0300

    ajuste de errors

--- src/Components/Main/index.jsx
+++ src/Components/Main/index.jsx
@@ -22,57 +22,62 @@ const Main = () => {
     const [fixedFilter, setFixedFilter] = useState(false)
 
     useEffect(() => {
-        let timeoutId;
 
         const fetchData = async () => {
             const url = 'https://games-test-api-81e9fb0d564a.herokuapp.com/api/data';
             const email = 'juliocst1993@gmail.com';
 
-            timeoutId = setTimeout(() => {
-                toast.error('O servidor demorou para responder, tente mais tarde');
+            const cancelToken = axios.CancelToken;
+            let cancel;
+
+            //setando uma função para executar o chamado de error
+            const timeoutId = setTimeout(() => {
+               cancel('O servidor demorou para responder, tente mais tarde')
             }, 5000);
 
-            try {
-                const response = await axios.get(url, {
-                    headers: {
+
+            let requisicao = await axios.get(url, {
+                cancelToken: new cancelToken(function executor(c) {
+                    cancel = c;
+                }),
+                headers: {
                     'dev-email-address': email,
-                    },
-            });
+                },
+            }).then(response => {
+                clearTimeout(timeoutId);
+                setDataGames(response.data);
+                setShowData(response.data)
 
-            clearTimeout(timeoutId);
+                const Genre = getOptions('genre',response.data)
+                const Plata = getOptions('platform', response.data)
+                setOptGenre(Genre)
+                setOptPlataform(Plata)
+                setIsLoad(false)
 
-            setDataGames(response.data);
-            setShowData(response.data)
+            }).catch(error => {
+                console.log(error)
+                if(error.code?.code === 'ERR_CANCELED') {
+                    toast.error(error.message)
+                    clearTimeout(timeoutId);
+                    setIsLoad('Error')
+                    requisicao = ''
+                } else if (
+                    error.response &&
+                    [500, 502, 503, 504, 507, 508, 509].includes(error.response.status)
+                ) {
+                    toast.error('O servidor falhou em responder, tente recarregar a página');
+                    return setIsLoad('Error')      
+                } else {
+                    toast.error('O servidor não conseguirá responder por agora, tente voltar novamente mais tarde');
+                    setIsLoad('Error')
+                    
+                }
+            })
 
-            const Genre = getOptions('genre',response.data)
-            const Plata = getOptions('platform', response.data)
-            setOptGenre(Genre)
-            setOptPlataform(Plata)
-            setIsLoad(false)
-
-            } catch (error) {
-            clearTimeout(timeoutId);
-
-            if (
-                error.response &&
-                [500, 502, 503, 504, 507, 508, 509].includes(error.response.status)
-            ) {
-                toast.error('O servidor falhou em responder, tente recarregar a página');
-                setIsLoad('Error')
-                
-            } else {
-                toast.error('O servidor não conseguirá responder por agora, tente voltar novamente mais tarde');
-                setIsLoad('Error')
-                
-            }
-            }
+            
         };
 
         fetchData();
-
-        return () => {
-            clearTimeout(timeoutId);
-        };
     }, []);
 
     function getOptions (type, array){
