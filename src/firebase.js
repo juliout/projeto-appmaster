8846c2fcd63343482f1ad118a7aaa4f3bc79e65d
juliout <juliocst1993@gmail.com>
2023-07-15 21:48:05 -0300

    add firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
-
+//aqui abaixo vão as configurações do firebase, não é certo eu por aqui, mas como é para um teste, eu não me preocupei com isso. desculpe;
 const firebaseConfig = {
-   apiKey: import.meta.env.VITE_APIKEY,
-   authDomain: import.meta.env.VITE_AUTH,
-   projectId: import.meta.env.VITE_PROJECTID,
-   storageBucket: import.meta.env.VITE_STORAGE,
-   messagingSenderId: import.meta.env.VITE_MSGID,
-   appId: import.meta.env.VITE_APPID,
+   apiKey: 'AIzaSyApsuG51gmELzPC9O6xcOZuxEj4QzP-Eg0',
+   authDomain: 'app-master-3bc7f.firebaseapp.com',
+   projectId: 'app-master-3bc7f',
+   storageBucket: 'app-master-3bc7f.appspot.com',
+   messagingSenderId: '129940435410',
+   appId: '1:129940435410:web:9ef350c8bd37fdcc8d180b',
 };
 
 // Initialize Firebase
